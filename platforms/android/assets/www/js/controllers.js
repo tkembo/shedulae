791fa10aeb5643d13809614dad24ec7deb6a8a1a
Tawanda Kembo <tkembo@gmail.com>
2015-07-14 07:40:55 +0300

    removing platforms from .gitignore

---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,182 @@
+angular.module('starter.controllers', ['ionic', 'firebase'])
+
+.controller('DashCtrl', function($scope, $firebase) {
+
+  // connect to firebase 
+  var ref = new Firebase("https://schedulae.firebaseio.com/days");  
+  var fb = $firebase(ref);
+
+  // sync as object 
+  var syncObject = fb.$asObject();
+
+  // three way data binding
+  syncObject.$bindTo($scope, 'days');
+  
+    // function to set the default data
+  $scope.reset = function() {    
+
+    fb.$set({
+      monday: {
+        name: 'Monday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          }
+        }
+      },
+      tuesday: {
+        name: 'Tuesday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          }
+        }
+      },
+      wednesday: {
+        name: 'Wednesday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          }
+        }
+      },
+      thursday: {
+        name: 'Thursday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          }
+        }
+      },
+      Friday: {
+        name: 'Friday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          }
+        }
+      }
+    });    
+
+  };
+ 
+})
+
+.controller('ChatsCtrl', function($scope) {
+   
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
